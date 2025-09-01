80531152808f00e0ce402adbf613b558824cd1f0
Lucas <psj@planfit.ai>
2025-09-01 15:03:41 +0900

    Revert "Revert "feat :: version up && react-native new Architecture support (#1)" (#2)"
    This reverts commit 4a5479f8eedef8545a35b4613fcdd96fc5330d8b.

--- android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
+++ android/src/main/java/com/doublesymmetry/trackplayer/service/MusicService.kt
@@ -764,9 +764,7 @@ class MusicService : HeadlessJsTaskService() {
 
     @MainThread
     private fun emit(event: String, data: Bundle? = null) {
-        reactNativeHost.reactInstanceManager.currentReactContext
-            ?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-            ?.emit(event, data?.let { Arguments.fromBundle(it) })
+        reactContext.emitDeviceEvent(event, data?.let { Arguments.fromBundle(it) })
     }
 
     @MainThread
@@ -774,9 +772,7 @@ class MusicService : HeadlessJsTaskService() {
         val payload = Arguments.createArray()
         data.forEach { payload.pushMap(Arguments.fromBundle(it)) }
 
-        reactNativeHost.reactInstanceManager.currentReactContext
-            ?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-            ?.emit(event, payload)
+        reactContext.emitDeviceEvent(event, payload)
     }
 
     override fun getTaskConfig(intent: Intent?): HeadlessJsTaskConfig {
